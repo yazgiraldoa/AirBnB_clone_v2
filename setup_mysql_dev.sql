fe0a39e232068c6cf599297cd2af88e0f4c30947
yazgiraldoa <edygiraldo.97@gmail.com>
2021-12-20 16:46:57 -0500

    add setup for database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Create a data base hbnb_dev_db and new user
+-- The password of hbnb_dev should be set to hbnb_dev_pwd
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
