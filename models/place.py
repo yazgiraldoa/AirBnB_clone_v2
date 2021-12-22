f94c0bebef4fae6414e8c99c1bb2f64bfc6a5129
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-21 23:55:52 -0500

    V2 fix

--- models/place.py
+++ models/place.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from re import I
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
@@ -8,10 +7,10 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("city.id"), nullable=False)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
+    description = Column(String(1024))
     number_rooms = Column(Integer, default=0, nullable=False)
     number_bathrooms = Column(Integer, default=0, nullable=False)
     max_guest = Column(Integer, default=0, nullable=False)
