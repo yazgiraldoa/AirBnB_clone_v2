1b4d6581d4404f3129c80371bcb15ad102a52a3f
Julian-Carlosama <cromocelcarlosama@gmail.com>
2021-12-21 23:01:31 -0500

    V3 Update argumnets

--- models/user.py
+++ models/user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = "users"
