e53eaeda2c22cd38cb95a782e83d8f71afae4929
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-13 18:08:43 -0500

    return path

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,16 +6,18 @@ AirBnB Clone repo, using the function do_pack.
 """
 from fabric.api import local
 from datetime import datetime
+from os import path
 
 
 def do_pack():
     """
     Function that compress a folder (to targz) with Fabric
     """
-    try:
-        name = "web_static_" + datetime.now().strftime('%Y%m%d%H%M%S')
-        local("mkdir -p versions")
-        local("tar -czvf versions/" + name + ".tgz web_static")
-        return name
-    except Exception:
+    name = "web_static_" + datetime.now().strftime('%Y%m%d%H%M%S')
+    local("mkdir -p versions")
+    local("tar -czvf versions/" + name + ".tgz web_static")
+    f_path = "versions/{}.tgz".format(name)
+    if (path.exists(f_path) and path.getsize(f_path)) > 0:
+        return f_path
+    else:
         return None
