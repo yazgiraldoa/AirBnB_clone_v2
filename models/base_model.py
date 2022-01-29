a89a0c59f11aded5c99d19be9db6dcf5c736fb33
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-29 17:59:47 -0500

    add attribute validations

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,10 @@ class BaseModel:
                     setattr(self, key, value)
             if 'id' not in kwargs:
                 self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
