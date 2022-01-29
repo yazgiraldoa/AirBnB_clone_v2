939d50c2d2c1a431e1ff2abcf1315e69ac39e36a
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-29 18:04:33 -0500

    add scripts that starts a Flask web app

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Function that returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function that returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Function that returns C + input text"""
+    output = text.replace('_', ' ')
+    return f"C {escape(output)}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Function that returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Function that returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """Function that returns C + input text"""
+    output = text.replace('_', ' ')
+    return f"C {escape(output)}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """Function that returns Python + input text"""
+    output = text.replace('_', ' ')
+    return f"Python {escape(output)}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_it_number(n):
+    """Function that returns a number only if it's an integer"""
+    if isinstance(n, int):
+        return f"{escape(n)} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Function that returns an HTML template"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
