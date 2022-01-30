8e550bd4a2ab8824cea5ede541ea3416151f6407
yazgiraldoa <edygiraldo.97@gmail.com>
2022-01-30 13:57:02 -0500

    fix session maker

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,8 +79,9 @@ class DBStorage:
     def reload(self):
         """Configure and create a session"""
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine,
-                               expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
         self.__session = Session()
 
     def close(self):
