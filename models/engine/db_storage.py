6ca86c4d5ca8f3e42c771fc20100892d0fe08b39
yazgiraldoa <edygiraldo.97@gmail.com>
2022-02-02 12:11:11 -0500

    try other add() version

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
-""" This script defines a class DBStorage """
-from models import base_model
-from models.base_model import Base
+""" State Module for HBNB project """
+from os import getenv
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, scoped_session
-from os import environ
-from models.user import User
+from sqlalchemy.sql.schema import MetaData
+from models.base_model import Base
+from models.amenity import Amenity
 from models.city import City
 from models.place import Place
-from models.state import State
-from models.amenity import Amenity
 from models.review import Review
+from models.state import State
+from models.user import User
 
 
-class DBStorage():
-    """ This class defines attributes and methods """
+user = getenv('HBNB_MYSQL_USER')
+password = getenv('HBNB_MYSQL_PWD')
+host = getenv('HBNB_MYSQL_HOST')
+database = getenv('HBNB_MYSQL_DB')
+env = getenv('HBNB_ENV')
+config = 'mysql+mysqldb://{}:{}@{}/{}'.format(
+    user, password, host, database)
+
+
+class DBStorage:
+    """
+    Class that configures a database
+    """
     __engine = None
     __session = None
     clases_objects = [User, State, City, Amenity, Place, Review]
 
     def __init__(self):
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}\
-'.format(environ['HBNB_MYSQL_USER'], environ['HBNB_MYSQL_PWD\
-'], environ['HBNB_MYSQL_HOST'], environ['HBNB_MYSQL_DB'], pool_pre_ping=True))
-        if 'HBNB_ENV' in environ and environ['HBNB_ENV'] == 'test':
-            Base.metadata.drop_all(self.__engine)
+        self.__engine = create_engine(config, pool_pre_ping=True)
+
+        """ Drop all tables"""
+        if env == "test":
+            metadata = MetaData(self.__engine)
+            metadata.reflect()
+            metadata.drop_all()
 
     def all(self, cls=None):
         """ This method returns all objects depending of the class name """
@@ -41,23 +54,20 @@ class DBStorage():
         return result
 
     def new(self, obj):
-        """ This method adds the object to the current database session """
+        """ Add new elements """
         self.__session.add(obj)
 
     def save(self):
-        """ This method commits all changes of the current database session """
+        """Commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """ This method deletes from the current
-            database session obj if not None """
-        item = self.__session.query(obj['__class__']).filter(obj['name']).one()
-        item
-        self.__session.delete(item)
+        """Delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
 
     def reload(self):
-        """ This method creates all tables in the database
-            and creates the current database session """
+        """Configure and create a session"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
@@ -65,5 +75,5 @@ class DBStorage():
         self.__session = Session()
 
     def close(self):
-        """  """
+        """Public method to close the session"""
         self.__session.close()
